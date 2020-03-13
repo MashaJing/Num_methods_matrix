f52adddf49e8c77a92b5ceacf040bed42f1abaf7
MashaJing <53327043+MashaJing@users.noreply.github.com>
2020-03-14 02:33:50 +0700

    Update Main.cpp

--- Laba_1/LabaNM_1/Main.cpp
+++ Laba_1/LabaNM_1/Main.cpp
@@ -5,15 +5,10 @@
 #include <time.h>  
 #include "CSIR.h"
 
-#define EPS 1e+3
-
 using namespace std;
 
 int main()
-{
-	try
-	{
-		//Задание 1
+{		//Р—Р°РґР°РЅРёРµ 1
 		//CSIR a("data\\");
 
 		CSIR x;
@@ -21,81 +16,92 @@ int main()
 		vector<vector<double>> T;
 		vector<double> y, result;
 		
-		//Задание 2
+		//Р—Р°РґР°РЅРёРµ 2
 		T = x.to_dens();
 
-		//Задание 3, 4
+		//Р—Р°РґР°РЅРёРµ 3, 4
 		for (int i = 0; i < x.size; i++)
 			y.push_back(rand()%10+1);
 
 		unsigned int fTimeStart = clock();
-		x.matrix_mult_vector(y, result); //CSIR формат
+		x.matrix_mult_vector(y, result); //CSIR С„РѕСЂРјР°С‚
 		unsigned int fTimeStop = clock();
 
 		unsigned int timeProc1 = fTimeStop - fTimeStart;
 
 		fTimeStart = clock();
-		densit_mult_vector(y, T, result); //плотный формат
+		densit_mult_vector(y, T, result); //РїР»РѕС‚РЅС‹Р№ С„РѕСЂРјР°С‚
 		fTimeStop = clock();
 
 		unsigned int timeProc2 = fTimeStop - fTimeStart;
 		cout << "CSIR: " << timeProc1 << endl;
 		cout << "Densit: " << timeProc2 << endl;
 
-		//Задание 5
+		//Р—Р°РґР°РЅРёРµ 5
 		y.clear();
 		y.push_back(1);
 		for (int i = 0; i < x.size - 1; i++)
 			y.push_back(0);
 
 
-		//цикл 1
+		//С†РёРєР» 1
 		double N = 0;
 		int ind = 0;
+
+	/*	try
+		{
+			while (1)
+			{
+				N = 0;
+				ind++;
+				x.matrix_mult_vector(y, result);
+
+				for (int i = 0; i < result.size(); i++)
+				{
+					result[i] *= result[i];
+					//if (N > 1e+307) cout << N << "  " << result[i] << endl;
+					if ((N >= DBL_MAX) || (result[i] >= DBL_MAX)) throw ind;
+					N += result[i];
+
+				}
+				N = sqrt(N);
+				cout << "N=" << N << " ";
+				cout << "x1=" << y[0] << endl;
+				if (y[0] >= DBL_MAX) throw ind;
+				y[0] *= 10;
+			}
+		}
+		catch (int ind)
+		{
+			cout << "Double overflow at iteration " << ind << endl;
+			system("pause");
+		}*/
+
+		//С†РёРєР» 2
+	try 
+	{
 		while (1)
 		{
+			N = 0;
 			ind++;
 			x.matrix_mult_vector(y, result);
-
 			for (int i = 0; i < result.size(); i++)
 			{
 				result[i] *= result[i];
-				if (N>1e+307) cout << DBL_MAX - result[i] - N << "  " << DBL_MAX - result[i] << "  "<< DBL_MAX - N << endl;
-				if (DBL_MAX - (N + result[i]) <= EPS) throw "Double overflow";
 				N += result[i];
-				
 			}
 			N = sqrt(N);
+			if (N == 0) throw ind;
+			if (y[0] == 0) throw ind;
 			cout << "N=" << N << " ";
-			cout << "x1=" << y[0] << endl;
-			if (y[0] >= DBL_MAX / 10) throw "Double overflow";
-			y[0] *= 10;
-		}
-		
-		//цикл 2
-	/*	while (1)
-		{
-			ind++;
-			int i = 0;
-			x.matrix_mult_vector(y, result);
-
-			while (i < result.size())
-			{
-				result[i] *= result[i];
-				if (fabs(result[i] - DBL_MIN) ) throw "Double underflow";
-				N = N + result[i];
-				i++;
-			}
-			N = sqrt(N);
-			cout << "N=" << N << " ";
-			cout << "x1=" << y[0] << endl;
+			cout << "y1=" << y[0] << endl;
 			y[0] /= 10;
-			if (y[0] == 0) throw "Double underflow";
-		}*/
+		
+		}
 	}
-	catch (exception & ex)
+	catch (int ind)
 	{
-		cout << ex.what() << endl;
+		cout << "Double underflow at iteration "<< ind << endl;
 		system("pause");
 	}
 }
